7ff71cdd321190c310de8a8d9bb146aa50e3a238
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-04-23 04:07:52 +0530

    Cors Add

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 // Import route
 const routes = require("./routes");
